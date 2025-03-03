258ff8ad37719bd9c69ce1665cbcb978a2fd703d
Dragos <dragosbercea@gmail.com>
2025-03-03 18:39:36 +1300

    added in the scroll view and a small navigation bar item to implement later on

--- AlarmClock/ContentView.swift
+++ AlarmClock/ContentView.swift
@@ -11,15 +11,39 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
+        NavigationStack {
             
-            AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
-            AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
-            AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
-            AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+            VStack {
+                
+                ScrollView {
+                    
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+                    AlarmPillView(time: "9:48 AM", date: "4 Monday 2025")
+
+                }
+                
+            }
+            .padding([.leading, .trailing], 16)
+            .navigationTitle("Alarm")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Alarm") { // TODO: make it an icon instead
+                        print("Take user to create alarm screen")
+                    }
+                }
+            }
             
         }
-        .padding([.leading, .trailing], 16)
+        
 
     }
     
