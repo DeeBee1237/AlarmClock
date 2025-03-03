390aa04ddc01a5c97e7794abc629156fe677015c
Dragos <dragosbercea@gmail.com>
2025-03-03 15:53:22 +1300

    Initial Commit

---
+++ AlarmClock/AlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockApp.swift
+//  AlarmClock
+//
+//  Created by Joseph on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmClockTests/AlarmClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockTests.swift
+//  AlarmClockTests
+//
+//  Created by Joseph on 03/03/2025.
+//
+
+import Testing
+@testable import AlarmClock
+
+struct AlarmClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
