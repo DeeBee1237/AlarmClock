e88ff8d3147bc6e87c76e9e0355a36f210238a2c
Dragos <dragosbercea@gmail.com>
2025-03-03 16:37:21 +1300

    added the first very rough bit of code for creating the pills that will display alarms

--- AlarmClock/ContentView.swift
+++ AlarmClock/ContentView.swift
@@ -8,15 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var moveToNewView = true
+
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("First Commit!")
+            alarmClock(time: "9:48 AM", date: "4 Monday 2025")
+            alarmClock(time: "9:48 AM", date: "4 Monday 2025")
+            alarmClock(time: "9:48 AM", date: "4 Monday 2025")
+            alarmClock(time: "9:48 AM", date: "4 Monday 2025")
         }
-        .padding()
     }
+    
+    
+    @ViewBuilder func alarmClock(time: String, date: String) -> some View {
+        
+        ZStack {
+
+            Color.gray
+
+            HStack {
+                
+                Text(time)
+                Text(date)
+                Toggle("", isOn: $moveToNewView)
+                
+            }
+        }
+        .cornerRadius(16)
+        .frame(height: 100)
+        .padding([.leading, .trailing], 16)
+        
+    }
+    
 }
 
 #Preview {
